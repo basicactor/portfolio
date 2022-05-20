e781d60aca64a3f6301d8503b8546ef90a5f0a48
basicactor <careergo357@gmail.com>
2022-05-20 11:30:52 +0900

    ビルド先ディレクトリ名変更

--- docs/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "C:/Users/fukuh/Documents/プログラミング開発/portfolio/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,11 @@ import { defineNuxtConfig } from "nuxt";
 
 //https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: "static",
   ssr: false,
-  buildDir: "docs",
   app: {
     baseURL: "/portfolio/",
+    buildAssetsDir: "nuxt",
   },
   css: ["@/assets/styles/tailwind.css"],
   build: {
