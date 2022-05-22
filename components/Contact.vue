0c020202901d6667b7816e648f3a303a676e44c9
basicactor <careergo357@gmail.com>
2022-05-23 02:35:57 +0900

    アイコン追加

--- components/Contact.vue
+++ components/Contact.vue
@@ -5,7 +5,13 @@ import SectionWrapper from './SectionWrapper.vue';
 <template>
   <SectionWrapper bgColor="bg-base-200">
     <template #title>Contact</template>
-    <template #content>TwitterでDMください</template>
+    <template #content>
+      <a class="w-12 inline-block" href="https://twitter.com/oneDock888"><img
+          src="@/assets/images/twitter-icon.svg" /></a>
+      <a class="w-12 inline-block" href="mailto:nekohack888&#64;gmail.com"><img
+          src="@/assets/images/gmail-icon.svg" /></a>
+      <p>TwitterでDMください</p>
+    </template>
 
   </SectionWrapper>
 
