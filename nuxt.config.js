3adf8ab92c71fae38d45c0d79dc8d12e6c47827a
basicactor <careergo357@gmail.com>
2022-05-22 22:26:18 +0900

    nuxt設定を環境分岐できるよう修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,18 @@
 import { defineNuxtConfig } from "nuxt";
 
+const app =
+  process.env.NODE_ENV == "production"
+    ? {
+        baseURL: "/portfolio/",
+        buildAssetsDir: "nuxt",
+      }
+    : {};
+
 //https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: "static",
   ssr: false,
-  // app: {
-  //   baseURL: "/portfolio/",
-  //   buildAssetsDir: "nuxt",
-  // },
+  app,
   css: ["@/assets/styles/tailwind.css", "@/assets/styles/common.scss"],
   build: {
     postcss: {
