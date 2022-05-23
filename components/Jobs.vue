070f2b9e64e258b5cddfeacb3eb24abbf90b0ba8
basicactor <careergo357@gmail.com>
2022-05-23 18:07:18 +0900

    レスポンシブ微調整

--- components/Jobs.vue
+++ components/Jobs.vue
@@ -48,24 +48,22 @@ const tableItems: Array<{
             <P>参加者と日時で検索すると空き日程候補が表示される。候補を選択すると会議室予約と参加者への会議依頼が一括で行える。</P>
             <h3 class="font-bold mt-4">開発環境</h3>
           </div>
-          <div class="overflow-auto">
-            <table class="table w-full">
-              <thead>
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th>項目</th>
+                <th>言語/プラットフォーム</th>
+              </tr>
+            </thead>
+            <tbody>
+              <template v-for="item in tableItems">
                 <tr>
-                  <th>項目</th>
-                  <th>言語/プラットフォーム</th>
+                  <td>{{ item.label }}</td>
+                  <td class="whitespace-normal">{{ item.detail }}</td>
                 </tr>
-              </thead>
-              <tbody>
-                <template v-for="item in tableItems">
-                  <tr>
-                    <td>{{ item.label }}</td>
-                    <td class="whitespace-normal">{{ item.detail }}</td>
-                  </tr>
-                </template>
-              </tbody>
-            </table>
-          </div>
+              </template>
+            </tbody>
+          </table>
         </div>
       </div>
     </template>
