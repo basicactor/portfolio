0e5117db66732bc1911cd86cd0e523c251e01aeb
basicactor <careergo357@gmail.com>
2022-05-23 15:08:14 +0900

    navbarバグ修正

--- components/Header.vue
+++ components/Header.vue
@@ -6,15 +6,16 @@ const isOpenSideBar = ref(false)
 <template>
   <header>
     <nav class="fixed top-0 z-10 w-screen">
-      <font-awesome-icon class="md:hidden p-4 absolute right-0 top-0" icon="bars" size="lg"
-        @click="() => isOpenSideBar = true" />
-      <ul class="hidden md:show absolute right-0 flex flex-row space-x-10 p-4">
+      <ul class="invisible md:visible absolute right-4 flex flex-row space-x-10 p-4">
         <template v-for="nav in navs">
           <li><a :href='`#${nav.toLowerCase()}`'>{{ nav }}</a></li>
         </template>
       </ul>
+      <font-awesome-icon class=" visible md:invisible p-4 absolute right-0 top-0" icon="bars" size="lg"
+        @click="() => isOpenSideBar = true" />
     </nav>
   </header>
+
   <div v-if="isOpenSideBar" class="fixed right-0 top-0 z-10 h-screen w-1/2 bg-white">
     <font-awesome-icon class="p-4 absolute right-0 top-0" icon="xmark" size="lg" @click="() => isOpenSideBar = false" />
 
