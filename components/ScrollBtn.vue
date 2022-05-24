c2ee24cf9680a2b35a8f5bc9e8b651ffd185c38f
basicactor <careergo357@gmail.com>
2022-05-24 17:43:31 +0900

    デザイン調整

--- components/ScrollBtn.vue
+++ components/ScrollBtn.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="pt-8 text-black tracking-widest 
+  <div class="invisible md:visible pt-8 text-gray-900 dark:text-gray-300 tracking-widest 
   transition-opacity duration-500 ease-in hover:text-opacity-50" href="#">
     <font-awesome-icon icon="angle-down" size="2x"
-      class="animate-bounce absolute top-0 right-0 left-0 mx-auto  text-gray-900" />
+      class="animate-bounce absolute top-0 right-0 left-0 mx-auto  text-gray-900 dark:text-gray-300" />
     <div class="py-2">Scroll</div>
   </div>
 </template>
