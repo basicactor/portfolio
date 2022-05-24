d00e7f7a77efeacee533ad29c72dd803c31800e3
basicactor <careergo357@gmail.com>
2022-05-24 15:31:19 +0900

    cardのダークモード設定変更

--- components/Card.vue
+++ components/Card.vue
@@ -8,7 +8,7 @@ const { badges, size, hasSubtitle } = defineProps<{
 </script>
 
 <template>
-  <div :class='`card w-${size ?? 72} bg-base-100 shadow-md`'>
+  <div :class='`card w-${size ?? 72} bg-base-100 dark:bg-gray-700 shadow-md`'>
     <div class="card-body p-6">
       <slot name="img" />
       <h2 class="font-bold text-xl text-center">
