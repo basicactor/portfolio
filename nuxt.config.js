ad58112968d8c6b093912a3e6c3bdceee6d353df
basicactor <careergo357@gmail.com>
2022-05-25 01:26:25 +0900

    ファビコン追加＆メタ情報追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,17 @@ const app =
 
 //https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // Headers of the page
+  head: {
+    title: "basicactor's Portfolio",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "@assets/images/favicon.ico" }, // ここを変更する
+    ],
+  },
   target: "static",
   ssr: false,
   app,
