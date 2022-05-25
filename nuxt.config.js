dc5740e65ea294202706903b3584fa038e2d4105
basicactor <careergo357@gmail.com>
2022-05-26 00:38:04 +0900

    設定バグ修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV == "production";
 
 const baseURL = isProduction ? "/portfolio/" : "";
 const buildAssetsDir = isProduction ? "/nuxt" : "";
-const href = isProduction ? "/basicactor/favicon.png" : "/favicon.png";
+const href = isProduction ? "/portfolio/favicon.png" : "/favicon.png";
 
 //https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
